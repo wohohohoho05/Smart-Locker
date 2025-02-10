6e7d6f9a1e7136f3e5d8afa20c6f2f3a94a5c5d0
wohohohoho05 <845871714@qq.com>
2025-02-10 21:42:16 +0800

    ➕新建info.h，分离常量与主程序

---
+++ include/info.h
@@ -0,0 +1,14 @@
+// WiFi配置
+// const char* ssid = "USER_1919810";
+// const char* password = "Ba+2Na=BaNaNa";
+const char* ssid = "2402";
+const char* password = "heyanhua2402";
+
+// OneNet MQTT配置 
+const char* mqttServer = "mqtts.heclouds.com";
+int mqttPort = 1883;  // 非SSL端口
+
+// 设备鉴权信息
+const char* PRODUCT_ID = "fhi0NS1Nvw";
+const char* DEVICE_ID = "dev1";
+const char* API_KEY = "version=2018-10-31&res=products%2Ffhi0NS1Nvw%2Fdevices%2Fdev1&et=2054535740&method=md5&sign=LM3uKKGDp36w9J5B%2BFVZyw%3D%3D";
--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,11 @@
 #include <WiFiS3.h>
 #include <ArduinoMqttClient.h>
 #include<Arduino_Json.h>
+#include<info.h>
 
 void connectWiFi();
 void sendDataToCloud();
 void onMqttMessage(int messageSize);
-
-// WiFi配置
-// const char* ssid = "USER_1919810";
-// const char* password = "Ba+2Na=BaNaNa";
-const char* ssid = "2402";
-const char* password = "heyanhua2402";
-
-// OneNet MQTT配置 
-const char* mqttServer = "mqtts.heclouds.com";
-int mqttPort = 1883;  // 非SSL端口
-
-// 设备鉴权信息
-const char* PRODUCT_ID = "fhi0NS1Nvw";
-const char* DEVICE_ID = "dev1";
-const char* API_KEY = "version=2018-10-31&res=products%2Ffhi0NS1Nvw%2Fdevices%2Fdev1&et=2054535740&method=md5&sign=LM3uKKGDp36w9J5B%2BFVZyw%3D%3D";
 
 String reply_topic = String("$sys/") + PRODUCT_ID + "/" + DEVICE_ID + "/thing/property/post/reply";
 String set_topic = String("$sys/") + PRODUCT_ID + "/" + DEVICE_ID + "/thing/property/set";
